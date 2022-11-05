95450569c2632e3aba436c5db626a87f040e093d
Lab-ERP-23\Lab-ERP23 <bagushot@gmail.com>
2022-11-05 17:22:16 +0700

    eventhandler

---
+++ app/src/main/java/com/example/pertemuan2/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.pertemuan2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pertemuan2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    var scoreA = 0
+    var scoreB = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.buttonPlusA.setOnClickListener {
+            scoreA = scoreA + 1
+            binding.tvScoreA.text = scoreA.toString()
+        }
+        binding.buttonMinA.setOnClickListener {
+            if (scoreA > 0) {
+                scoreA = scoreA - 1
+                binding.tvScoreA.text = scoreA.toString()
+            }
+        }
+        binding.buttonPlusB.setOnClickListener {
+            scoreB = scoreB + 1
+            binding.tvScoreB.text = scoreB.toString()
+        }
+
+        binding.buttonMinB.setOnClickListener{
+            if (scoreB > 0) {
+                scoreB = scoreB - 1
+                binding.tvScoreB.text = scoreB.toString()
+            }
+        }
+        binding.buttonReset.setOnClickListener {
+            scoreA = 0
+            scoreB = 0
+            binding.tvScoreA.text = scoreA.toString()
+            binding.tvScoreB.text = scoreB.toString()
+        }
+    }
+}
